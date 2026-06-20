1a7b52e99cc2ef2abbb2da8b157788c63d400e39
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-21 05:42:11 +0600

    Update prob10.py

--- Assignment1/prob10.py
+++ Assignment1/prob10.py
@@ -1,3 +1,9 @@
+"""
+i have taken a string and list then check
+if a specific string belong to string or a
+specific number belong to the list or not using
+'in' and 'not in'.
+"""
 str_trgt = 'python'
 li_trgt = [1,3,5,7,9]
 
