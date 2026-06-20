a46f7ff203df180ec968932837686660fdc15e00
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-21 03:53:23 +0600

    Create prob3.py

---
+++ Assignment1/prob3.py
@@ -0,0 +1,12 @@
+"""
+I printed the given data fast. then swap them using python swap method. 
+And printed the final value.
+"""
+
+a,b,c = 10, 20, 30
+
+print(f"Before:  a = {a}, b = {b}, c = {c}")
+
+a, c = c, a
+
+print(f"After: a = {a}, b = {b}, c = {c}")
