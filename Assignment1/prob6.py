835ff4cfa50cbb58407217da3de9ce9244f38050
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-21 05:00:57 +0600

    finished prob 6-7

---
+++ Assignment1/prob6.py
@@ -0,0 +1,10 @@
+a = 15
+b = 4
+
+print(f"Addition: { a+b }")
+print(f"Subtraction: { a-b }")
+print(f"Multiplication: { a*b }")
+print(f"Division: { a/b }")
+print(f"Floor division: { a//b }")
+print(f"Modulus: { a%b }")
+print(f"Exponentiation: { a**b }")
---
+++ Assignment1/prob7.py
@@ -0,0 +1,17 @@
+
+x = 100
+
+x -= 25
+print(f"After subtraction: {x}")
+
+x *= 2
+print(f"After multiplication: {x}")
+
+x //= 3
+print(f"After floor division: {x}")
+
+x %= 7
+print(f"After modulus: {x}")
+
+x **= 2
+print(f"After raise to power 2: {x}")
