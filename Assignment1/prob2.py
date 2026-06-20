3a29c59e7ebe97af3fc2f9098e9c460ef92dd44a
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-21 03:39:15 +0600

    Create prob2.py

---
+++ Assignment1/prob2.py
@@ -0,0 +1,10 @@
+"""I converted each given value using type casting - type(). 
+And print it with its type to verify.
+"""
+x = int(42)
+y = int(3.9)
+z = str(True)
+
+print(x, type(x))
+print(y, type(y))
+print(z, type(z))
