dd0426d4baa1e2001582a698382198e4e1aeec1e
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-21 05:41:39 +0600

    Create prob10.py

---
+++ Assignment1/prob10.py
@@ -0,0 +1,7 @@
+str_trgt = 'python'
+li_trgt = [1,3,5,7,9]
+
+print("Is 'py' inside 'python'?", 'py' in str_trgt)
+print("Is 'z' not in 'python'?", 'z' not in str_trgt)
+print("Is 7 in the list?", 7 in li_trgt)
+print("Is 4 not in the list?", 4 not in li_trgt)
