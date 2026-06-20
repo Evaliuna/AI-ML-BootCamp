cd8f23cf4cf1f7fa12aadf7d777df5411b73f8ef
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-20 21:31:34 +0600

    Create prob1.py

---
+++ Assignment1/prob1.py
@@ -0,0 +1,9 @@
+#I have taken 5 different variables with value and printed them in a single print()
+
+name = "Mosammat Samiea Jahan Eva"
+age = 22
+height = 1.62
+is_student = True
+nothing = None
+
+print(name, age, height, is_student, nothing)
