086876aee39fff8b8c826aa49e6b1a1236096f5c
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-22 05:49:08 +0600

    Update prob18.py

--- Assignment2/prob18.py
+++ Assignment2/prob18.py
@@ -1,3 +1,7 @@
+"""
+create a function count_down in loop range have -1 which helps to print the number reverse
+or downward and called the function with the number i want to print till 0.
+"""
 def count_down(n):
     for i in range(n, 0, -1):
         print(i)
