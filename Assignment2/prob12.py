4c577d228e38746597ac1231c86fb83dc719d525
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-22 05:12:19 +0600

    Create prob12.py

---
+++ Assignment2/prob12.py
@@ -0,0 +1,4 @@
+def add_numbers(a, b): 
+    return a + b  #returning the value of addition of the given numbers while calling
+
+print(add_numbers(3, 5)) #calling the function add_numbers with attributes
