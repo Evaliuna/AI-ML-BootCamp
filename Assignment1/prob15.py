8f803bb43146d8f1c1fa575f115fe019f5931a4b
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-21 19:21:27 +0600

    Create prob15.py

---
+++ Assignment1/prob15.py
@@ -0,0 +1,12 @@
+product = 'Laptop'
+price = 75000.5
+discount = 10
+
+f_string = f"Product: {product} | Price: {price:.2f} | Discount: {discount}%"
+print("f-string style:", f_string)
+
+format = "Product: {} | Price: {:.2f} | Discount: {}%".format(product, price, discount)
+print(".format() style:", format)
+
+percent = "Product: %s | Price: %.2f | Discount: %d%%" % (product, price, discount)
+print("% operator style:", percent)
