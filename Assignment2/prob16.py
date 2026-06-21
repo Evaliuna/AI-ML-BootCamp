1f0e177de85b4fc51134d32de4f13ceaedfd5073
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-22 05:39:31 +0600

    Create prob16.py

---
+++ Assignment2/prob16.py
@@ -0,0 +1,9 @@
+
+age = int(input("Enter your age: "))
+
+if age > 18:
+    print("You are an adult.")
+    if age > 60:
+        print("Status: Senior Citizen")
+else:
+    print("You are a minor.")
