b8e0bc00a16a0b04d5ad7a5361ab2816f9e03342
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-21 17:27:11 +0600

    Create prob13.py

---
+++ Assignment1/prob13.py
@@ -0,0 +1,6 @@
+sentence = 'Hello World Python'
+
+print("First 5 characters:", sentence[:5])
+print("Last 6 characters:", sentence[-6:])
+print("Every second character:", sentence[::2])
+print("Entire string reversed:", sentence[::-1])
