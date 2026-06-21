9f430281105c5e91e801c8b5aa4cf47e9d4cdba3
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-22 04:32:16 +0600

    Create prob5.py

---
+++ Assignment2/prob5.py
@@ -0,0 +1,7 @@
+"""
+i have taken a string input and find its length using len() and stroed it into total_characters
+and print it out.
+"""
+string = input("Enter a string: ")
+total_characters = len(string)
+print(f"Total number of characters (length): {total_characters}")
