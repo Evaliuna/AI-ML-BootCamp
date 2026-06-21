f61428c6f3fee2563415189fa253d162909bc5df
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-06-21 18:10:12 +0600

    Create prob14.py

---
+++ Assignment1/prob14.py
@@ -0,0 +1,9 @@
+word = 'Banana'
+
+print("find('a'):", word.find('a'))
+print("count('a'):", word.count('a'))
+print("startswith('Ba'):", word.startswith('Ba'))
+print("endswith('na'):", word.endswith('na'))
+print("Is 'Banana' a digit?:", word.isdigit())
+print("Is '12345' a digit?:", '12345'.isdigit())
+print("Is '123a5' a digit?:", '123a5'.isdigit())
