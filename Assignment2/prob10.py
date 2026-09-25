9daba1dfb1988f8e46b70831ef57041c7772fef0
Mosammat Samiea Jahan Eva <samieajahaneva9543@gmail.com>
2026-09-25 13:18:21 +0600

    Update prob10.py

--- Assignment2/prob10.py
+++ Assignment2/prob10.py
@@ -1,3 +1,7 @@
+"""
+i toke a total vari to do the summation. and a for loop with vari num which take each number and 
+perform the loop each time it added to total and finally printed total vari as sum of the range numbers.
+"""
 total = 0
 for num in range(1, 51):
     total += num
